e19deef6dfea87afe91f65cd73de49fac0f5f5de
Ashish <ashishDongol32@gmail.com>
2020-09-24 18:09:54 +0545

    splash screen completed

--- app/src/main/java/com/example/menu_app/Splash_ScreenActivity.java
+++ app/src/main/java/com/example/menu_app/Splash_ScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.menu_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_ScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Splash_ScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash__screen);
+
+        Handler handler = new Handler();
+        handler.postAtTime(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash_ScreenActivity.this,IntroActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
