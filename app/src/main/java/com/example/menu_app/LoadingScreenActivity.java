6ca635d661b01b7499d6e008815e3769e1dc99dc
Ashish <ashishDongol32@gmail.com>
2020-09-25 17:50:53 +0545

    loading screen progress

--- app/src/main/java/com/example/menu_app/LoadingScreenActivity.java
+++ app/src/main/java/com/example/menu_app/LoadingScreenActivity.java
@@ -3,12 +3,28 @@ package com.example.menu_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoadingScreenActivity extends AppCompatActivity {
+
+    TextView tvUserType, tvUsername;
+    ImageView ivChief, ivWaiter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
+
+        tvUserType = findViewById(R.id.userType);
+        tvUsername = findViewById(R.id.userName);
+        ivChief = findViewById(R.id.chiefPic);
+        ivWaiter = findViewById(R.id.waiterPic);
+
+        String a = "Waiter";
+        if (a == "Waiter"){
+            ivChief.setVisibility(View.INVISIBLE);
+        }
     }
 }
