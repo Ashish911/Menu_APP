bcf3ee44beff1c4d023d2625543d1d179e922509
Ashish <ashishDongol32@gmail.com>
2020-09-30 14:54:02 +0545

    order completed fragment done

---
+++ app/src/main/java/com/example/menu_app/adapter/CompletedOrderAdapter.java
@@ -0,0 +1,78 @@
+package com.example.menu_app.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.menu_app.R;
+import com.example.menu_app.model.Order;
+import com.example.menu_app.url.Url;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class CompletedOrderAdapter extends RecyclerView.Adapter<CompletedOrderAdapter.ViewHolder>{
+
+    private Context context;
+    private List<Order> orderList;
+
+    public CompletedOrderAdapter(Context context, List<Order> orderList) {
+        this.context = context;
+        this.orderList = orderList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_completed,parent,false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        final Order order = orderList.get(position);
+
+        final boolean confirmed = order.isConfirmed();
+        final boolean completed = order.isCompleted();
+
+        Toast.makeText(context, "" + completed, Toast.LENGTH_SHORT).show();
+
+        if (confirmed == true && completed == true){
+            Picasso.get().load(Url.base_url_image + orderList.get(position).getFoodImage()).into(holder.FoodImage);
+            holder.FoodName.setText(order.getFoodName());
+            holder.Quantity.setText("Quantity:  " + order.getQuantity());
+            holder.FullName.setText("For " + order.getFullName());
+        } else {
+            holder.FoodName.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return orderList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        ImageView FoodImage;
+        TextView FoodName,Quantity,FullName;
+        CircleImageView completed;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            FoodImage = itemView.findViewById(R.id.COrderedImage);
+            FoodName = itemView.findViewById(R.id.COrderedFoodName);
+            Quantity = itemView.findViewById(R.id.COrderedQuantity);
+            FullName = itemView.findViewById(R.id.COrderedFullName);
+            completed = itemView.findViewById(R.id.greenstatus);
+        }
+    }
+}
--- app/src/main/java/com/example/menu_app/ui/CompletedFragment.java
+++ app/src/main/java/com/example/menu_app/ui/CompletedFragment.java
@@ -1,21 +1,37 @@
 package com.example.menu_app.ui;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.menu_app.R;
+import com.example.menu_app.adapter.CompletedOrderAdapter;
+import com.example.menu_app.api.OrderAPI;
+import com.example.menu_app.model.Order;
+import com.example.menu_app.url.Url;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class CompletedFragment extends Fragment {
 
+    RecyclerView recyclerView;
 
     public CompletedFragment() {
         // Required empty public constructor
@@ -26,7 +42,30 @@ public class CompletedFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_completed, container, false);
+        View view = inflater.inflate(R.layout.fragment_completed, container, false);
+
+        recyclerView = view.findViewById(R.id.AllCompletedRecyclerView);
+
+
+        OrderAPI orderAPI = Url.getInstance().create(OrderAPI.class);
+        Call<List<Order>> listCall = orderAPI.getorder();
+        listCall.enqueue(new Callback<List<Order>>() {
+            @Override
+            public void onResponse(Call<List<Order>> call, Response<List<Order>> response) {
+                CompletedOrderAdapter completedOrderAdapter = new CompletedOrderAdapter(getActivity(), response.body());
+                recyclerView.setAdapter(completedOrderAdapter);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                completedOrderAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<Order>> call, Throwable t) {
+                Toast.makeText(getActivity(), "Error" +t.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
     }
 
 }
